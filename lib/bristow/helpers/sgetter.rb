180c5a56e6ce571cd820114959fc1e8c040dad39
Andrew Hampton <andrewwh@gmail.com>
2025-02-01 12:45:23 -0500

    feat: add sgetter helper
    sgetter makes creating classes that can be configured like this very
    easy:
    
    ```ruby
    class MyAgent < Agent
      name "James Bond"
    end
    ```
    
    In agent, you define `sgetter :agent` and it will give you the class
    level getter/setter and an instance getter.

---
+++ lib/bristow/helpers/sgetter.rb
@@ -0,0 +1,42 @@
+# A helper to define a getter/setter method
+# This will let you define a class level getter/setter method with an instance reader
+#
+# Example:
+# class Agent
+#   include Bristow::Sgetter
+#   sgetter :name
+#   sgetter :model, default: -> { Bristow.configuration.model }
+# end
+#
+# class Sydney < Agent
+#   name 'Sydney'
+# end
+#
+# sydney = Sydney.new
+# sydney.name # => 'Sydney'
+module Bristow
+  module Sgetter
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    module ClassMethods
+      def sgetter(attr, default: nil)
+        # Define the method that allows for both getter/setter syntax
+        define_singleton_method(attr) do |val = nil|
+          if val.nil?
+            return instance_variable_get("@#{attr}") if instance_variable_defined?("@#{attr}")
+            default.respond_to?(:call) ? default.call : default
+          else
+            instance_variable_set("@#{attr}", val)
+          end
+        end
+
+        # Instance-level reader
+        define_method(attr) do
+          instance_variable_get("@#{attr}")
+        end
+      end
+    end
+  end
+end
